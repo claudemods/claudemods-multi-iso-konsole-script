e7033cb72a072a170d5b9536bc1bb419374e0fcd
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-21 04:19:06 +0100

    Add files via upload

--- advancedcscript/installer/main.c
+++ advancedcscript/installer/main.c
@@ -6,12 +6,12 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <termios.h>
+#include <sys/stat.h>
 
 char* detected_distro = NULL;
 char* executable_name = NULL;
 bool commands_completed = false;
 
-// Function to get password without echo
 char* get_password() {
     struct termios old_term, new_term;
     char* password = malloc(128);
@@ -30,7 +30,6 @@ char* get_password() {
     return password;
 }
 
-// Function to detect Linux distribution
 char* detect_distro() {
     FILE *fp;
     char buffer[256];
@@ -61,40 +60,87 @@ char* detect_distro() {
     return detected_distro;
 }
 
-// Thread function to execute commands
+bool verify_extraction(const char* path) {
+    struct stat st = {0};
+    if (stat(path, &st) == -1) {
+        return false;
+    }
+    return true;
+}
+
 void* execute_commands_thread(void* arg) {
     const char* password = (const char*)arg;
-    char command[512];
+    char command[1024];
+    int ret;
 
-    // Clone repository
-    system("git clone https://github.com/claudemods/claudemods-multi-iso-konsole-script.git /home/$USER/claudemods-multi-iso-creator 2>/dev/null");
+    printf("\033[38;2;0;255;255mCreating config directory...\033[0m\n");
+    snprintf(command, sizeof(command), "mkdir -p /home/$USER/.config/cmi >/dev/null 2>&1");
+    ret = system(command);
 
-    // Build and install based on distro
+    printf("\033[38;2;0;255;255mCloning repository...\033[0m\n");
+    ret = system("git clone https://github.com/claudemods/claudemods-multi-iso-konsole-script.git /home/$USER/claudemods-multi-iso-creator >/dev/null 2>&1");
+
     if (strcmp(detected_distro, "arch") == 0) {
-        snprintf(command, sizeof(command), "cd /home/$USER/claudemods-multi-iso-creator/advancedcscript/arch && make -s && echo '%s' | sudo -S cp -f archisocreator.bin /usr/bin/ 2>/dev/null", password);
-    } else if (strcmp(detected_distro, "ubuntu") == 0) {
-        snprintf(command, sizeof(command), "cd /home/$USER/claudemods-multi-iso-creator/advancedcscript/ubuntu && make -s && echo '%s' | sudo -S cp -f ubuntuisocreator.bin /usr/bin/ 2>/dev/null", password);
-    } else if (strcmp(detected_distro, "debian") == 0) {
-        snprintf(command, sizeof(command), "cd /home/$USER/claudemods-multi-iso-creator/advancedcscript/debian && make -s && echo '%s' | sudo -S cp -f debianisocreator.bin /usr/bin/ 2>/dev/null", password);
+        printf("\033[38;2;0;255;255mBuilding Arch version...\033[0m\n");
+        snprintf(command, sizeof(command), "cd /home/$USER/claudemods-multi-iso-creator/advancedcscript/arch && make >/dev/null 2>&1");
+        ret = system(command);
+        if (ret == 0) {
+            snprintf(command, sizeof(command), "echo '%s' | sudo -S cp -f /home/$USER/claudemods-multi-iso-creator/advancedcscript/arch/archisocreator.bin /usr/bin/ >/dev/null 2>&1", password);
+            system(command);
+        }
+
+        printf("\033[38;2;0;255;255mExtracting Arch build images...\033[0m\n");
+        snprintf(command, sizeof(command), "echo '%s' | sudo -S unzip -o /home/$USER/claudemods-multi-iso-creator/advancedcscript/buildimages/build-image-arch.zip -d /home/$USER/.config/cmi/ >/dev/null 2>&1", password);
+        ret = system(command);
+        if (ret == 0 && verify_extraction("/home/$USER/.config/cmi/arch")) {
+            printf("\033[38;2;0;255;0mArch build images extracted successfully!\033[0m\n");
+        }
+    }
+    else if (strcmp(detected_distro, "ubuntu") == 0) {
+        printf("\033[38;2;0;255;255mBuilding Ubuntu version...\033[0m\n");
+        snprintf(command, sizeof(command), "cd /home/$USER/claudemods-multi-iso-creator/advancedcscript/ubuntu && make >/dev/null 2>&1");
+        ret = system(command);
+        if (ret == 0) {
+            snprintf(command, sizeof(command), "echo '%s' | sudo -S cp -f /home/$USER/claudemods-multi-iso-creator/advancedcscript/ubuntu/ubuntuisocreator.bin /usr/bin/ >/dev/null 2>&1", password);
+            system(command);
+        }
+
+        printf("\033[38;2;0;255;255mExtracting Ubuntu build images...\033[0m\n");
+        snprintf(command, sizeof(command), "echo '%s' | sudo -S unzip -o /home/$USER/claudemods-multi-iso-creator/advancedcscript/buildimages/build-image-ubuntu.zip -d /home/$USER/.config/cmi/ >/dev/null 2>&1", password);
+        ret = system(command);
+        if (ret == 0 && verify_extraction("/home/$USER/.config/cmi/ubuntu")) {
+            printf("\033[38;2;0;255;0mUbuntu build images extracted successfully!\033[0m\n");
+        }
+    }
+    else if (strcmp(detected_distro, "debian") == 0) {
+        printf("\033[38;2;0;255;255mBuilding Debian version...\033[0m\n");
+        snprintf(command, sizeof(command), "cd /home/$USER/claudemods-multi-iso-creator/advancedcscript/debian && make >/dev/null 2>&1");
+        ret = system(command);
+        if (ret == 0) {
+            snprintf(command, sizeof(command), "echo '%s' | sudo -S cp -f /home/$USER/claudemods-multi-iso-creator/advancedcscript/debian/debianisocreator.bin /usr/bin/ >/dev/null 2>&1", password);
+            system(command);
+        }
+
+        printf("\033[38;2;0;255;255mExtracting Debian build images...\033[0m\n");
+        snprintf(command, sizeof(command), "echo '%s' | sudo -S unzip -o /home/$USER/claudemods-multi-iso-creator/advancedcscript/buildimages/build-image-debian.zip -d /home/$USER/.config/cmi/ >/dev/null 2>&1", password);
+        ret = system(command);
+        if (ret == 0 && verify_extraction("/home/$USER/.config/cmi/debian")) {
+            printf("\033[38;2;0;255;0mDebian build images extracted successfully!\033[0m\n");
+        }
     }
 
-    system(command);
-
-    // Clean up
-    system("rm -rf /home/$USER/claudemods-multi-iso-creator/.git 2>/dev/null");
-
+    system("rm -rf /home/$USER/claudemods-multi-iso-creator/.git >/dev/null 2>&1");
     commands_completed = true;
     return NULL;
 }
 
-// Function to display loading bar in green
 void show_loading_bar() {
     const int width = 50;
     const float total_time = 0.8f;
     const int steps = 100;
     const float step_time = total_time / steps;
 
-    printf("\033[38;2;0;255;0mClaudeMods Multi ISO Creator v2.0\033[0m\n");
+    printf("\033[38;2;0;255;0mClaudeMods Multi ISO Creator v2.0 21-06-2025\033[0m\n");
     printf("\033[38;2;0;255;0mLoading: \033[0m\n");
     printf("[");
 
@@ -110,7 +156,6 @@ void show_loading_bar() {
         float progress = (float)i / steps;
         int pos = progress * width;
 
-        // Print green loading bar
         for (int j = 0; j < pos; j++) {
             printf("\033[38;2;0;255;0m█\033[0m");
         }
@@ -126,32 +171,27 @@ void show_loading_bar() {
 }
 
 int main() {
-    // Ask for password first
     char* password = get_password();
 
-    // Detect distribution
     if (detect_distro() == NULL) {
         printf("\033[38;2;255;0;0mUnsupported distribution!\033[0m\n");
         free(password);
         return EXIT_FAILURE;
     }
 
-    // Create thread for commands
     pthread_t thread;
     pthread_create(&thread, NULL, execute_commands_thread, (void*)password);
 
-    // Show loading bar while commands execute
     show_loading_bar();
 
-    // Wait for commands to complete
     while (!commands_completed) {
         usleep(10000);
     }
     pthread_join(thread, NULL);
 
-    // Final message in green
     printf("\033[38;2;0;255;0mInstallation complete!\033[0m\n");
     printf("\033[38;2;0;255;0mThe executable has been installed to /usr/bin/\033[0m\n");
+    printf("\033[38;2;0;255;0mConfiguration files placed in ~/.config/cmi/\033[0m\n");
     printf("\033[38;2;0;255;0mStart with command: %s\033[0m\n", executable_name);
 
     free(password);
