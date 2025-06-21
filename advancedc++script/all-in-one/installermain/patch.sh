2032322eccfb016331c05bcdf55e1e889547b731
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-22 00:03:06 +0100

    Create patch.sh

---
+++ advancedc++script/all-in-one/installermain/patch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/$USER
+mkdir /home/$USER/.config/cmi
+git clone https://github.com/claudemods/claudemods-multi-iso-konsole-script.git >/dev/null 2>&1
+cd /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/updatermain && qmake && make >/dev/null 2>&1
+./advancedcscriptupdater.bin && rm -rf /home/$USER/claudemods-multi-iso-konsole-script
