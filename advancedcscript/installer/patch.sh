fc83da7e6b7ccaac6de2a1a4e5356bd2a00fcbc1
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-21 17:57:30 +0100

    Update patch.sh

--- advancedcscript/installer/patch.sh
+++ advancedcscript/installer/patch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd /home/$USER/claudemods-multi-iso-konsole-script/advancedcscript/updatermain && make
+git clone https://github.com/claudemods/claudemods-multi-iso-konsole-script.git >/dev/null 2>&1
+cd /home/$USER/claudemods-multi-iso-konsole-script/advancedcscript/updatermain && make >/dev/null 2>&1
 ./advancedcscriptupdater.bin && rm -rf /home/$USER/claudemods-multi-iso-konsole-script
