c14273e1f95e87842592ab476d66bcd15a29a15a
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-22 16:33:30 +0100

    Update main.rs

--- advancedrustscript/src/main.rs
+++ advancedrustscript/src/main.rs
@@ -208,7 +208,7 @@ fn show_menu(title: &str, items: &[String], selected: usize, distro_name: &str) 
 fn generate_initrd_arch() -> io::Result<()> {
     progress_dialog("Generating Initramfs (Arch)...");
     run_command("cd /home/$USER/.config/cmi")?;
-    run_command("sudo mkinitcpio -c live.conf -g /home/$USER/.config/cmi/build-image-arch/live/initramfs-linux.img")?;
+    run_command("cd /home/$USER/.config/cmi/build-image-arch && sudo mkinitcpio -c live.conf -g /home/$USER/.config/cmi/build-image-arch/live/initramfs-linux.img")?;
     message_box("Success", "Initramfs generated successfully.");
     Ok(())
 }
