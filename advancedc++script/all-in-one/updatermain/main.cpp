2aa971ca1c26dc57522ae5dfe1234de20a409483
claudemods <73653396+claudemods@users.noreply.github.com>
2025-06-26 10:53:07 +0100

    support for ubuntu and neon

--- advancedc++script/all-in-one/updatermain/main.cpp
+++ advancedc++script/all-in-one/updatermain/main.cpp
@@ -60,7 +60,7 @@ void* execute_update_thread(void* /*arg*/) {
         strcpy(current_version, "not installed");
     }
 
-    // 3. DETECT DISTRO (EXPLICIT CACHYOS SUPPORT)
+    // 3. DETECT DISTRO (EXPLICIT CACHYOS AND KDE NEON SUPPORT)
     try {
         std::string distro_output = run_command("cat /etc/os-release | grep '^ID=' | cut -d'=' -f2 | tr -d '\"'");
         if (distro_output == "arch" || distro_output == "cachyos") {
@@ -69,6 +69,8 @@ void* execute_update_thread(void* /*arg*/) {
             strcpy(detected_distro, "ubuntu");
         } else if (distro_output == "debian") {
             strcpy(detected_distro, "debian");
+        } else if (distro_output == "neon") { // KDE Neon detection
+            strcpy(detected_distro, "neon");
         }
     } catch (...) {
         strcpy(detected_distro, "unknown");
@@ -83,7 +85,7 @@ void* execute_update_thread(void* /*arg*/) {
         } catch (...) {
             strcpy(downloaded_version, "unknown");
         }
-    } else if (strcmp(detected_distro, "ubuntu") == 0) {
+    } else if (strcmp(detected_distro, "ubuntu") == 0 || strcmp(detected_distro, "neon") == 0) {
         try {
             std::string version_output = run_command(
                 "cat /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/version/ubuntu/version.txt");
@@ -109,13 +111,12 @@ void* execute_update_thread(void* /*arg*/) {
     if (strcmp(detected_distro, "arch") == 0 || strcmp(detected_distro, "cachyos") == 0) {
         silent_command("cp /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/version/arch/version.txt /home/$USER/.config/cmi/");
         silent_command("unzip -o /home/$USER/claudemods-multi-iso-konsole-script/advancedcscript/buildimages/build-image-arch.zip -d /home/$USER/.config/cmi/");
-    } else if (strcmp(detected_distro, "ubuntu") == 0) {
+    } else if (strcmp(detected_distro, "ubuntu") == 0 || strcmp(detected_distro, "neon") == 0) {
         silent_command("cp /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/version/ubuntu/version.txt /home/$USER/.config/cmi/");
         silent_command("unzip -o /home/$USER/claudemods-multi-iso-konsole-script/advancedcscript/buildimages/build-image-ubuntu.zip -d /home/$USER/.config/cmi/");
     } else if (strcmp(detected_distro, "debian") == 0) {
         silent_command("cp /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/version/debian/version.txt /home/$USER/.config/cmi/");
         silent_command("unzip -o /home/$USER/claudemods-multi-iso-konsole-script/advancedcscript/buildimages/build-image-debian.zip -d /home/$USER/.config/cmi/");
-        
     }
 
     // FINAL STEPS WITH DISTRO DETECTION
@@ -126,9 +127,16 @@ void* execute_update_thread(void* /*arg*/) {
         silent_command("cp -r /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/calamares-per-distro /home/$USER/.config/cmi");
         silent_command("cp -r /home/$USER/claudemods-multi-iso-konsole-script/guide/readme.txt /home/$USER/.config/cmi");
         silent_command("cp -r /home/$USER/claudemods-multi-iso-konsole-script/changesc++.txt /home/$USER/.config/cmi");
-    } else if (strcmp(detected_distro, "ubuntu") == 0 || strcmp(detected_distro, "debian") == 0) {
-        // Ubuntu/Debian-specific commands
-        silent_command("cd /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one && qmake6 && make >/dev/null 2>&1");
+    } else if (strcmp(detected_distro, "ubuntu") == 0 || strcmp(detected_distro, "neon") == 0) {
+        // Ubuntu/KDE Neon-specific commands
+        silent_command("cd /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one && qmake && make >/dev/null 2>&1");
+        silent_command("sudo cp /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/cmi.bin /usr/bin/cmi.bin");
+        silent_command("cp -r /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/calamares-per-distro /home/$USER/.config/cmi");
+        silent_command("cp -r /home/$USER/claudemods-multi-iso-konsole-script/guide/readme.txt /home/$USER/.config/cmi");
+        silent_command("cp -r /home/$USER/claudemods-multi-iso-konsole-script/changesc++.txt /home/$USER/.config/cmi");
+    } else if (strcmp(detected_distro, "debian") == 0) {
+        // Debian-specific commands
+        silent_command("cd /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one && qmake && make >/dev/null 2>&1");
         silent_command("sudo cp /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/cmi.bin /usr/bin/cmi.bin");
         silent_command("cp -r /home/$USER/claudemods-multi-iso-konsole-script/advancedc++script/all-in-one/calamares-per-distro /home/$USER/.config/cmi");
         silent_command("cp -r /home/$USER/claudemods-multi-iso-konsole-script/guide/readme.txt /home/$USER/.config/cmi");
