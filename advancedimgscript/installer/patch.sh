25e38105682fec3664edfa90031772698bd97364
claudemods <73653396+claudemods@users.noreply.github.com>
2025-07-23 23:20:31 +0100

    Create patch.sh

---
+++ advancedimgscript/installer/patch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to detect the distribution
+detect_distro() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        DISTRO=$ID
+    else
+        echo "Cannot detect the distribution. Exiting."
+        exit 1
+    fi
+}
+
+# Main script starts here
+cd /home/$USER >/dev/null 2>&1
+mkdir -p /home/$USER/.config/cmi >/dev/null 2>&1
+
+# Detect the distribution
+detect_distro
+
+# Conditional logic based on the detected distribution
+if [[ "$DISTRO" == "arch" || "$DISTRO" == "cachyos" ]]; then
+    # Commands for Arch/CachyOS
+    git clone https://github.com/claudemods/claudemods-multi-iso-konsole-script.git  >/dev/null 2>&1
+    cd /home/$USER/claudemods-multi-iso-konsole-script/advancedimgscript && qmake6 && make >/dev/null 2>&1
+    ./cmiimg && rm -rf /home/$USER/claudemods-multi-iso-konsole-script
+else
+    echo "Unsupported distribution: $DISTRO"
+    exit 1
+fi
