62036e43ad198caf26a1870437f372cb9fe06529
claudemods <73653396+claudemods@users.noreply.github.com>
2025-07-23 23:46:31 +0100

    Update main.cpp

--- advancedimgscript/main.cpp
+++ advancedimgscript/main.cpp
@@ -75,7 +75,7 @@ void printBanner() {
 ╚█████╔╝███████╗██║░░██║╚██████╔╝██████╔╝███████╗██║░╚═╝░██║╚█████╔╝██████╔╝██████╔╝
 ░╚════╝░╚══════╝╚═╝░░░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚═════╝░
 )" << COLOR_RESET << std::endl;
-std::cout << COLOR_CYAN << "claudemods arch btrfs/ext4 img iso creator v2.01 23-07-2025" << COLOR_RESET << std::endl << std::endl;
+std::cout << COLOR_CYAN << " Advanced C++ Arch Img Iso Script Beta v2.01 23-07-2025" << COLOR_RESET << std::endl << std::endl;
 }
 
 void showDiskUsage() {
