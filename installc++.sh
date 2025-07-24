f4e5b9cc4e20f33129416125a443df6c4a7f5cf5
claudemods <73653396+claudemods@users.noreply.github.com>
2025-07-24 10:57:45 +0100

    Update installc++.sh

--- installc++.sh
+++ installc++.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/claudemods/claudemods-multi-iso-konsole-script/main/advancedc++script/all-in-one-devbranch/installermain/patch.sh)"
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/claudemods/claudemods-multi-iso-konsole-script/main/advancedimgscript/installer/patch.sh)"
