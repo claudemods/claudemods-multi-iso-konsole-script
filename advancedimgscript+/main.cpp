161861a567e474c22b0843383f3c5cb89b3e3ca8
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-04 12:54:40 +0100

    click for more
    new option to edit boot text
    also fixed menu options blocking create image or create iso 
    until all check boxes are set
    date updated

--- advancedimgscript+/main.cpp
+++ advancedimgscript+/main.cpp
@@ -450,7 +450,7 @@ void printBanner() {
 ╚█████╔╝███████╗██║░░██║╚██████╔╝██████╔╝███████╗██║░╚═╝░██║╚█████╔╝██████╔╝██████╔╝
 ░╚════╝░╚══════╝╚═╝░░░░░░╚═════╝░╚══════╝░╚══════╝╚═╝░░░░░╚═╝░╚════╝░╚═════╝░╚═════╝░
 )" << COLOR_RESET << std::endl;
-std::cout << COLOR_CYAN << " Advanced C++ Arch Img Iso Script+ Beta v2.02 03-10-2025" << COLOR_RESET << std::endl;
+std::cout << COLOR_CYAN << " Advanced C++ Arch Img Iso Script+ Beta v2.02 04-10-2025" << COLOR_RESET << std::endl;
 
 {
     std::lock_guard<std::mutex> lock(time_mutex);
