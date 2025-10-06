93e845648865bdc12ba74fc3eca98b7f0b6f8348
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-06 20:12:01 +0100

    Update patch.sh

--- advancedimgscript+/installer/patch.sh
+++ advancedimgscript+/installer/patch.sh
@@ -22,15 +22,33 @@ detect_distro
 if [[ "$DISTRO" == "arch" || "$DISTRO" == "cachyos" ]]; then
     # Commands for Arch/CachyOS
     echo installing dependencies before git clone and install
+    echo 3
+    echo 2
+    echo 1
     echo updating pacman database
+    echo 3
+    echo 2
+    echo 1
     sudo pacman -Sy
     echo installing dependencies
+    echo 3
+    echo 2
+    echo 1
     sudo pacman -S --needed --noconfirm git rsync squashfs-tools xorriso grub dosfstools unzip nano arch-install-scripts bash-completion erofs-utils findutils jq libarchive libisoburn lsb-release lvm2 mkinitcpio-archiso mkinitcpio-nfs-utils mtools nbd pacman-contrib parted procps-ng pv python sshfs syslinux xdg-utils zsh-completions kernel-modules-hook virt-manager qt6-tools btrfs-progs e2fsprogs f2fs-tools xfsprogs xfsdump cmake
     echo git cloning repository
+    echo 3
+    echo 2
+    echo 1
     git clone https://github.com/claudemods/claudemods-multi-iso-konsole-script.git  >/dev/null 2>&1
     echo building installer
+    echo 3
+    echo 2
+    echo 1
     cd /home/$USER/claudemods-multi-iso-konsole-script/advancedimgscript+/updatermain && qmake6 && make >/dev/null 2>&1
     echo installing
+    echo 3
+    echo 2
+    echo 1
     ./advancedcscriptupdater.bin && rm -rf /home/$USER/claudemods-multi-iso-konsole-script
 else
     echo "Unsupported distribution: $DISTRO"
