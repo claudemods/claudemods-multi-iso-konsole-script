62e5a7f1de74de7519b02612beb74b1c3c1a9c84
claudemods <73653396+claudemods@users.noreply.github.com>
2026-06-16 00:58:17 +0100

    changes im going to improve the erofs script now

--- v3.0.x-cmiadvanced/v3.0-cmiadvanced/dev-branch/cloner.h
+++ v3.0.x-cmiadvanced/v3.0-cmiadvanced/dev-branch/cloner.h
@@ -75,28 +75,6 @@ public:
         return true;
     }
 
-    // Create SquashFS for another drive with xz compression
-    static bool createSquashFSForDrive(const std::string& inputDir, const std::string& outputFile) {
-        std::string command = "sudo mksquashfs " + inputDir + " " + outputFile +
-        " -noappend -comp xz -b 256K -Xbcj x86 " +
-        "-e etc/udev/rules.d/70-persistent-cd.rules " +
-        "-e etc/udev/rules.d/70-persistent-net.rules " +
-        "-e etc/mtab " +
-        "-e etc/fstab " +
-        "-e dev/* " +
-        "-e proc/* " +
-        "-e sys/* " +
-        "-e tmp/* " +
-        "-e run/* " +
-        "-e mnt/* " +
-        "-e media/* " +
-        "-e lost+found " +
-        "-e temp_clone_mount";
-
-        execute_command(command, true);
-        return true;
-    }
-
     // Create checksum file for the image
     static bool createChecksum(const std::string& filename) {
         std::string command = "sudo sha512sum " + filename + " > " + filename + ".sha512";
@@ -155,105 +133,10 @@ public:
         std::cout << COLOR_GREEN << "Current system cloned successfully using bind mount!" << COLOR_RESET << std::endl;
     }
 
-    // Clone another drive
-    static void cloneAnotherDrive(const std::string& cloneDir) {
-        std::cout << COLOR_GREEN << "\nAvailable drives:" << COLOR_RESET << std::endl;
-        execute_command("lsblk -f -o NAME,FSTYPE,SIZE,MOUNTPOINT | grep -v 'loop'", true);
-
-        std::string drive = getUserInput("Enter drive to clone (e.g., /dev/sda2): ");
-        if (drive.empty()) {
-            std::cerr << COLOR_RED << "No drive specified!" << COLOR_RESET << std::endl;
-            return;
-        }
-
-        std::string checkCmd = "ls " + drive + " > /dev/null 2>&1";
-        if (system(checkCmd.c_str()) != 0) {
-            std::cerr << COLOR_RED << "Drive " + drive + " does not exist!" << COLOR_RESET << std::endl;
-            return;
-        }
-
-        std::string tempMountPoint = "/mnt/temp_clone_mount";
-
-        if (!isDeviceMounted(drive)) {
-            if (!mountDevice(drive, tempMountPoint)) {
-                std::cerr << COLOR_RED << "Failed to mount " + drive + "!" << COLOR_RESET << std::endl;
-                return;
-            }
-        } else {
-            std::string mountCmd = "mount | grep " + drive + " | awk '{print $3}'";
-            FILE* fp = popen(mountCmd.c_str(), "r");
-            if (fp) {
-                char mountPath[256];
-                if (fgets(mountPath, sizeof(mountPath), fp)) {
-                    tempMountPoint = mountPath;
-                    tempMountPoint.erase(tempMountPoint.find_last_not_of("\n") + 1);
-                }
-                pclose(fp);
-            }
-        }
-
-        std::cout << COLOR_GREEN << "Creating SquashFS from " << drive << "..." << COLOR_RESET << std::endl;
-
-        std::string outputDir = getOutputDirectory();
-        std::string finalImgPath = outputDir + "/" + FINAL_IMG_NAME;
-
-        // Create SquashFS directly from the mounted drive with exclusions
-        createSquashFSForDrive(tempMountPoint, finalImgPath);
-
-        if (!isDeviceMounted(drive) || system(("mount | grep " + drive + " | grep " + tempMountPoint).c_str()) == 0) {
-            execute_command("sudo umount " + tempMountPoint, true);
-            execute_command("sudo rmdir " + tempMountPoint, true);
-        }
-
-        createChecksum(finalImgPath);
-        printFinalMessage(finalImgPath);
-
-        std::cout << COLOR_GREEN << "Drive " << drive << " cloned successfully!" << COLOR_RESET << std::endl;
-    }
-
-    // Clone folder or file
-    static void cloneFolderOrFile(const std::string& cloneDir) {
-        std::cout << COLOR_CYAN << "\nClone Folder or File" << COLOR_RESET << std::endl;
-        std::cout << COLOR_YELLOW << "Enter the path to a folder or file you want to clone." << COLOR_RESET << std::endl;
-        std::cout << COLOR_YELLOW << "Examples:" << COLOR_RESET << std::endl;
-        std::cout << COLOR_YELLOW << "  - Folder: /home/" << USERNAME << "/Documents" << COLOR_RESET << std::endl;
-        std::cout << COLOR_YELLOW << "  - File: /home/" << USERNAME << "/file.txt" << COLOR_RESET << std::endl;
-
-        std::string sourcePath = getUserInput("Enter folder or file path to clone: ");
-        if (sourcePath.empty()) {
-            std::cerr << COLOR_RED << "No path specified!" << COLOR_RESET << std::endl;
-            return;
-        }
-
-        std::string checkCmd = "sudo test -e " + sourcePath + " > /dev/null 2>&1";
-        if (system(checkCmd.c_str()) != 0) {
-            std::cerr << COLOR_RED << "Source path does not exist: " << sourcePath << COLOR_RESET << std::endl;
-            return;
-        }
-
-        std::string userfilesDir = cloneDir + "/home/userfiles";
-        execute_command("sudo mkdir -p " + userfilesDir, true);
-
-        std::cout << COLOR_CYAN << "Cloning " << sourcePath << " to " << userfilesDir << "..." << COLOR_RESET << std::endl;
-
-        std::string rsyncCmd = "sudo rsync -aHAXSr --numeric-ids --info=progress2 " +
-        sourcePath + " " + userfilesDir + "/";
-
-        execute_command(rsyncCmd, true);
-
-        std::cout << COLOR_GREEN << "Successfully cloned " << sourcePath << " to " << userfilesDir << "!" << COLOR_RESET << std::endl;
-
-        std::string listCmd = "sudo ls -la " + userfilesDir + " | head -20";
-        std::cout << COLOR_CYAN << "Contents of userfiles directory:" << COLOR_RESET << std::endl;
-        execute_command(listCmd, true);
-    }
-
     // Show clone options menu
     static void showCloneOptionsMenu(bool allCheckboxesChecked, const std::string& cloneDirConfig) {
         std::vector<std::string> items = {
             "Clone Current System (as it is now)",
-            "Clone Another Drive (e.g., /dev/sda2)",
-            "Clone Folder or File",
             "Back to Main Menu"
         };
 
@@ -298,18 +181,10 @@ public:
                             cloneCurrentSystem(cloneDir);
                             break;
                         case 1:
-                            cloneAnotherDrive(cloneDir);
-                            break;
-                        case 2:
-                            cloneFolderOrFile(cloneDir);
-                            std::cout << COLOR_YELLOW << "Folder/File cloned to " << cloneDir << "/home/userfiles" << COLOR_RESET << std::endl;
-                            std::cout << COLOR_YELLOW << "You can now create an ISO that includes these files." << COLOR_RESET << std::endl;
-                            break;
-                        case 3:
                             return;
                     }
 
-                    if (selected != 3) {
+                    if (selected != 1) {
                         std::cout << COLOR_GREEN << "\nPress any key to continue..." << COLOR_RESET;
                         getch();
                     }
