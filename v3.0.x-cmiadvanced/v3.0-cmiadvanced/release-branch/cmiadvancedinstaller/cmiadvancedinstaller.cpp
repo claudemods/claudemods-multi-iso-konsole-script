21315ae02e54cebb376ad5acd99e0e3f39f02970
claudemods <73653396+claudemods@users.noreply.github.com>
2026-07-03 16:53:21 +0100

    more fixes
    Added mounting of efivars for chroot environments in GRUB installation functions.

--- v3.0.x-cmiadvanced/v3.0-cmiadvanced/release-branch/cmiadvancedinstaller/cmiadvancedinstaller.cpp
+++ v3.0.x-cmiadvanced/v3.0-cmiadvanced/release-branch/cmiadvancedinstaller/cmiadvancedinstaller.cpp
@@ -192,28 +192,33 @@ void copy_system(const string& efi_part, const string& img_fs_type) {
 }
 
 void install_grub_ext4(const string& drive) {
+    // Mount efivars to make them available in chroot
+    execute_command("mount --bind /sys/firmware/efi/efivars /mnt/sys/firmware/efi/efivars 2>/dev/null || true");
     execute_command("mount --bind /dev /mnt/dev");
     execute_command("mount --bind /dev/pts /mnt/dev/pts");
     execute_command("mount --bind /proc /mnt/proc");
     execute_command("mount --bind /sys /mnt/sys");
     execute_command("mount --bind /run /mnt/run");
-    execute_command("mount -t efivarfs efivarfs /sys/firmware/efi/efivars/");
     
     // Configure GRUB with proper kernel parameters (from shell script)
     string kernel_args = "root=PARTUUID=$(lsblk -dno PARTUUID " + drive + "2)";
     
     execute_command("chroot /mnt /bin/bash -c \""
+    "modprobe efivarfs 2>/dev/null || true; "
+    "mount -t efivarfs efivarfs /sys/firmware/efi/efivars 2>/dev/null || true; "
     "echo 'GRUB_DEFAULT=0' > /etc/default/grub; "
     "echo 'GRUB_TIMEOUT=3' >> /etc/default/grub; "
     "echo 'GRUB_TIMEOUT_STYLE=menu' >> /etc/default/grub; "
-    "echo 'GRUB_CMDLINE_LINUX=\\\\" + kernel_args + "\\\"' >> /etc/default/grub; "
-    "genfstab -U /; "
+    "echo 'GRUB_CMDLINE_LINUX=\\\"" + kernel_args + "\\\"' >> /etc/default/grub; "
+    "genfstab -U / >> /etc/fstab; "
     "grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck; "
     "grub-mkconfig -o /boot/grub/grub.cfg; "
     "mkinitcpio -P\"");
 }
 
 void install_grub_btrfs(const string& drive) {
+    // Mount efivars to make them available in chroot
+    execute_command("mount --bind /sys/firmware/efi/efivars /mnt/sys/firmware/efi/efivars 2>/dev/null || true");
     execute_command("touch /mnt/etc/fstab");
     execute_command("mount --bind /dev /mnt/dev");
     execute_command("mount --bind /dev/pts /mnt/dev/pts");
@@ -225,6 +230,8 @@ void install_grub_btrfs(const string& drive) {
     string kernel_args = "root=PARTUUID=$(lsblk -dno PARTUUID " + drive + "2) rootflags=subvol=@ rootfstype=btrfs";
     
     execute_command("chroot /mnt /bin/bash -c \""
+    "modprobe efivarfs 2>/dev/null || true; "
+    "mount -t efivarfs efivarfs /sys/firmware/efi/efivars 2>/dev/null || true; "
     "echo 'GRUB_DEFAULT=0' > /etc/default/grub; "
     "echo 'GRUB_TIMEOUT=3' >> /etc/default/grub; "
     "echo 'GRUB_TIMEOUT_STYLE=menu' >> /etc/default/grub; "
